4e5c2db03b975f940207d0a36f358345cb8fca82
Whooperdars <kimdariuspanis@gmail.com>
2022-09-23 01:33:51 +0800

    email validation

--- code/client/src/pages/Contacts.js
+++ code/client/src/pages/Contacts.js
@@ -3,6 +3,7 @@ import React from 'react'
 import '../styles/Contacts.css';
 import { useState } from 'react'; 
 import moment from 'moment';
+import validator from 'email-validator'; 
 
 const Contacts = ({socket}) => {
   const [fullName, setFullName] = useState("");
@@ -14,16 +15,26 @@ const Contacts = ({socket}) => {
 
     if(fullName !== "" && emailAddress !== "" && message !== ""){
         
-        const userContactData = {
-            fullName: fullName, 
-            emailAddress: emailAddress, 
-            message: message, 
-            time: moment().format('MMMM Do YYYY, h:mm:ss a')    
-        }
+        // checks valid emails 
+        if(validator.validate(emailAddress)){
+            console.log("valid email"); 
+
+            // prepare data to save
+            const userContactData = {
+                fullName: fullName, 
+                emailAddress: emailAddress, 
+                message: message, 
+                time: moment().format('MMMM Do YYYY, h:mm:ss a')    
+            }
 
             // save data to database
-        await socket.emit("save-contact-user", userContactData); 
-  
+             await socket.emit("save-contact-user", userContactData); 
+        } 
+        else { 
+            console.log("invalid email");
+            setEmailAddress(""); 
+            return;
+        }
     }
 
     setFullName(""); 
@@ -40,19 +51,32 @@ const Contacts = ({socket}) => {
             </div>
 
             <div className='fields'>
-                <input type="text" value={fullName} placeholder='Full name' onChange={(e) => setFullName(e.target.value)} />
-                <input type="text" value={emailAddress} placeholder='Email address' onChange={(e) => setEmailAddress(e.target.value)}/>
-            </div>   
-            
-            <textarea 
-            name="Message" 
-            value={message} 
-            placeholder="Message" 
-            cols="107" rows="15" 
-            onChange={(e) => setMesssage(e.target.value)}
-            onKeyPress={(e) => e.key === "Enter" && saveResponse()}
+                <input 
+                    type="text" 
+                    value={fullName} 
+                    placeholder='Full name' 
+                    onChange={(e) => setFullName(e.target.value)}
+                     onKeyPress={(e) => e.key === "Enter" && saveResponse()}
+                    />
                 
-            ></textarea>
+                <input 
+                    type="text" 
+                    value={emailAddress} 
+                    placeholder='Email address' 
+                    onChange={(e) => setEmailAddress(e.target.value)}
+                     onKeyPress={(e) => e.key === "Enter" && saveResponse()}
+                    />
+            </div>   
+
+            <textarea 
+                name="Message" 
+                value={message} 
+                placeholder="Message" 
+                cols="107" rows="15" 
+                onChange={(e) => setMesssage(e.target.value)}
+                onKeyPress={(e) => e.key === "Enter" && saveResponse()}
+                    
+                ></textarea>
 
             <div className='send-btn'>
                 <button 
